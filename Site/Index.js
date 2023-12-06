c1821aef6cdfe2c8c5abc147a0fd446bee9c780f
Florian IMBAUD <imbaud.florian@orange.fr>
2023-12-06 14:32:06 +0000

    Ajout de login.js et changements de login.html

--- Site/Index.js
+++ Site/Index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { resolve } = require ('path');
 const { PrismaClient } = require('@prisma/client')
 
 const app = express();
 const prisma = new PrismaClient();
 const port = 3010;
 
+app.get("/", (req, res) => {
+    res.sendFile(resolve(__dirname,"Template/login.html"))
+})
+
+app.get("/dashboard.html", (req, res) => {
+    res.sendFile(resolve(__dirname,"Template/dashboard.html"))
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
---
+++ Site/login.js
@@ -0,0 +1,15 @@
+async function LoginUser() {
+    console.log("BABA");
+    var log_email = document.registration.email; 
+    var log_pswd = document.registration.password;
+    if (log_email && log_pswd)
+    {
+        const logUser = await prisma.utilisateurs.findUnique({
+            where: {
+                email: log_email,
+                password: log_pswd
+            }
+        })
+    }
+    console.log("BABA2");
+}
