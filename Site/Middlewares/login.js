4ec75c28069763740134c6d8bc5c58bc11bbf308
Florian IMBAUD <imbaud.florian@orange.fr>
2023-12-12 09:54:59 +0000

    Le login marche

--- Site/Middlewares/login.js
+++ Site/Middlewares/login.js
@@ -1,10 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function LoginUser(prisma, req, res) {
-    console.log("ENTREE dans LoginUser()");
-    var log_email = req.query.email;
-    var log_pswd = req.query.password;
+async function loginUser(req, res) {
+    console.log("ENTREE dans loginUser()");
+    const log_email = req.body.email;
+    const log_pswd = req.body.password;
+
     if (log_email && log_pswd) {
         try {
             const logUser = await prisma.utilisateurs.findUnique({
@@ -13,14 +14,16 @@ async function LoginUser(prisma, req, res) {
                     password: log_pswd
                 }
             });
+            console.log("L'utilisateur est :");
             console.log(logUser); // Assuming you want to log the user details
         } catch (error) {
             console.error("Error retrieving user:", error);
         }
     }
+
     console.log(log_email);
     console.log(log_pswd);
-    console.log("SORTIE de LoginUser()");
+    console.log("SORTIE de loginUser()");
 }
 
-module.exports = { LoginUser };
+module.exports = { loginUser };
---
+++ Site/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { resolve } = require('path');
+const { PrismaClient } = require('@prisma/client')
+
+const { loginUser } = require('./Middlewares/login');
+const { bodyParserMiddleware } = require('./Middlewares/middleware');
+
+const app = express();
+const prisma = new PrismaClient();
+const port = 3010;
+
+app.get("/", (req, res) => {
+    res.sendFile(resolve(__dirname, "Template/login.html"));
+});
+
+app.get("/login.html", (req, res) => {
+    res.sendFile(resolve(__dirname, "Template/login.html"));
+});
+
+app.get("/styles.css", (req, res) => {
+    res.sendFile(resolve(__dirname, "Template/styles.css"));
+});
+
+app.get("/inscription.html", (req, res) => {
+    res.sendFile(resolve(__dirname, "Template/inscription.html"));
+});
+
+app.get("/dashboard.html", (req, res) => {
+    res.sendFile(resolve(__dirname, "Template/dashboard.html"));
+});
+
+app.post("/login", bodyParserMiddleware, (req, res) => {
+    loginUser(req, res);
+    res.sendFile(resolve(__dirname, "Template/dashboard.html"));
+});
+
+app.get("/login.js", (req, res) => {
+    res.sendFile(resolve(__dirname, "login.js"));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
