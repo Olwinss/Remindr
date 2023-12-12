cafe21ad6f9846d0435780c0b077724c4ae9039f
Florian IMBAUD <imbaud.florian@orange.fr>
2023-12-12 10:43:27 +0000

    Update index.js / login.js / login.html

--- Site/Middlewares/login.js
+++ Site/Middlewares/login.js
@@ -6,16 +6,22 @@ async function loginUser(req, res) {
     const log_pswd = req.body.password;
 
     if (log_email && log_pswd) {
-        try {
-            const logUser = await prisma.utilisateurs.findUnique({
-                where: {
-                    email: log_email,
-                    password: log_pswd
-                }
-            });
-        } catch (error) {
-            console.error("Error retrieving user:", error);
+        const logUser = await prisma.utilisateurs.findUnique({
+            where: {
+                email: log_email,
+                password: log_pswd
+            }
+        });
+
+        if (logUser) {
+            // L'utilisateur a été trouvé
+            console.log("L'utilisateur est :", logUser);
+        } else {
+            // Aucun utilisateur trouvé
+            console.log("Aucun utilisateur trouvé pour l'email et le mot de passe fournis.");
         }
+    } else {
+        console.error("Les informations d'identification sont incomplètes.");
     }
 }
 
--- Site/index.js
+++ Site/index.js
@@ -10,7 +10,7 @@ const app = express();
 const prisma = new PrismaClient();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.sendFile(resolve(__dirname, "Template/login.html"));
