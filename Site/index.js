88f1ef151c5303776bf1dfe064a67b7127f61136
Florian IMBAUD <imbaud.florian@orange.fr>
2023-12-12 10:49:53 +0000

    Update index.js

--- Site/index.js
+++ Site/index.js
@@ -53,5 +53,5 @@ app.get("/login.js", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
